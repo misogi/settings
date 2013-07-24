9a67efb9fcf33d3a1d64e4087277126877d31d57
misogi <dev@misogi.info>
2013-07-24 10:37:07 +0900

    add vimrc to install it

--- install_dot_file.sh
+++ install_dot_file.sh
@@ -3,8 +3,9 @@
 rm ~/.zshenv
 rm ~/.zshrc
 rm ~/.gitconfig 
+rm ~/.vimrc
 
 ln -s $(pwd)/dot.zshenv ~/.zshenv
 ln -s $(pwd)/dot.zshrc ~/.zshrc
 ln -s $(pwd)/dot.gitconfig ~/.gitconfig
-
+ln -s $(pwd)/dot.vimrc ~/.vimrc
