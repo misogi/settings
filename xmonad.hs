836e6a4752d564dd10af3c7509765c4f5854b1cb
misogi <dev@misogi.info>
2013-11-19 10:32:56 +0900

    disable focusing on mouse track

--- xmonad.hs
+++ xmonad.hs
@@ -2,8 +2,9 @@ import XMonad
 import XMonad.Util.Run(spawnPipe)
 
 main = do
-  xmproc <- spawnPipe "xmobar"
+  
   xmonad $ defaultConfig
     {
-      terminal = "urxvt"
+      terminal = "urxvt",
+      focusFollowsMouse = False
     }
